1731fa42ed31c97ff22f2427496a468950c7a0bb
Maxim-Komogortsev <helgurskoggur@gmail.com>
2023-08-28 19:28:37 +1000

    first try to plain diff

--- gendiff/scripts/gen_diff.py
+++ gendiff/scripts/gen_diff.py
@@ -1,5 +1,9 @@
 import argparse
+from pathlib import Path
+import json
 
+
+BASEDIR: Path = Path(__file__).parent.parent
 
 def main():
     parser = argparse.ArgumentParser(
@@ -7,9 +11,44 @@ def main():
         )
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    print(args)
+    return generate_diff(args.first_file, args.second_file)
+     
 
+
+def json_load(path):
+    fname = Path(path).resolve()
+    with open(fname) as f:
+        json_obj = json.load(f)
+    return json_obj
+        
+def generate_diff(file1, file2):
+    data1 = json_load(file1)
+    data2 = json_load(file2)
+    agregate_data = data1.keys() | data2.keys()
+    sorted_data = sorted(agregate_data, key=lambda x: x[0])
+    strout = '{\n'
+    for i in sorted_data:
+
+        if i in data1 and i in data2:
+            if data1[i] != data2[i]:
+                strout += f' - {i}: {data1[i]}\n'\
+                        f' + {i}: {data2[i]}\n'
+            else:
+                strout += f'   {i}: {data1[i]}\n'
+            continue
+
+        if i in data1:
+            strout += f' - {i}: {data1[i]}\n'
+        
+        if i in data2:
+            strout += f' + {i}: {data2[i]}\n'
+
+    strout += '}'
+    
+    return strout
+        
 
 if __name__ == '__main__':
     main()
